01e03d705649955d8201bc7aff7d8bc43930e962
Clay Bell <clay.bell@etix.com>
2022-05-21 21:07:59 -0700

    Added some typescript support

---
+++ src/constants.ts
@@ -0,0 +1,27 @@
+
+export const PAYLOAD_STATE = 'state';
+export const PAYLOAD_REFRESH_TOKEN = 'refresh_token';
+
+// HTTP Header Constants
+export const HEADER_CONTENT_TYPE = 'application/x-www-form-urlencoded';
+export const HEADER_PREFIX_BASIC = 'Basic ';
+export const HEADER_AUTHORIZATION = 'Authorization';
+
+// HTTP Request
+export const PAYLOAD_VALUE_CLIENT_CREDENTIALS = 'client_credentials';
+export const PAYLOAD_VALUE_AUTHORIZATION_CODE = 'authorization_code';
+
+// Web End point
+export const OAUTHENVIRONMENT_WEBENDPOINT_PRODUCTION = 'https://auth.ebay.com/oauth2/authorize';
+export const OAUTHENVIRONMENT_WEBENDPOINT_SANDBOX = 'https://auth.sandbox.ebay.com/oauth2/authorize';
+
+// API End Point
+export const OAUTHENVIRONMENT_APIENDPOINT_SANDBOX = 'https://api.sandbox.ebay.com/identity/v1/oauth2/token';
+export const OAUTHENVIRONMENT_APIENDPOINT_PRODUCTION = 'https://api.ebay.com/identity/v1/oauth2/token';
+
+// Scopes
+export const CLIENT_CRED_SCOPE = 'https://api.ebay.com/oauth/api_scope';
+
+// Environments
+export const PROD_ENV = 'PRODUCTION';
+export const SANDBOX_ENV = 'SANDBOX';
